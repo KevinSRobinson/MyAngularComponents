04f9ae546ee308ea8920409b532b44b88d83fa3c
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-04-03 17:54:31 +0100

    ffff

--- my-angular-components.js
+++ my-angular-components.js
@@ -1,8 +1,8 @@
-var app = angular.module("myComponents", [])
-
 var myTextBox = {
     template : "<h1>Yexybox</h1>"    
 };
 
-app.component("myTextBox", myTextBox);
+
+angular.module("myComponents", []).component("myTextBox", myTextBox);
+
 
