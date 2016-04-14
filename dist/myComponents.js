6b2b40c953442c1a4655ee0cb19f4b93894e74e9
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-04-14 15:28:45 +0100

    fixed Modal Buttons

--- dist/myComponents.js
+++ dist/myComponents.js
@@ -271,14 +271,22 @@
 	    bindings: {
 	        save: "&save",
 	        close: "&close",
-	        savetext: "@",
-	        hidesave: "@",
+	        saveText: "@",
+	        hideSave: "@",
 	    },
+	    controllerAs: "vm",
+	      controller: function(){
+	        var vm = this;
+	        
+	        if(vm.savetext== undefined){
+	          vm.savetext= "Save";
+	        } 
+	      },
 	    template: ["<div class='modal-footer'>",
 	        "<span><button type='submit' id='save' class='btn btn-primary btn-large pull-left'",
-	        " ng-click='$ctrl.save()'>{{$ctrl.savetext}}</button>",
+	        " ng-click='vm.save()'>{{vm.savetext}}</button>",
 	        "<button type='button'  class='btn btn-default pull-left'",
-	        "ng-click='$ctrl.close()'>Close</button>",
+	        "ng-click='vm.close()'>Close</button>",
 	        "</span>",
 	        "</div>"].join("")
 	};
