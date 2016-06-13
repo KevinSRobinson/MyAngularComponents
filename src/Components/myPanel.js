7ee09cd1f3df5e68d4bc04a3a7a1bee9affc406f
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-06-13 10:06:31 +0100

    fixed

--- src/Components/myPanel.js
+++ src/Components/myPanel.js
@@ -9,7 +9,7 @@ var myPanel = {
     },
     controllerAs:"vm",
     template: "<div class='panel panel-default {{vm.style}}'>" +
-    "<div class='panel-heading' id='{{vm.fieldName}}'><i class='fa fa-{{vm.icon}} fa-{{vm.iconSize}}x></i>{{vm.title}}</div>" +
+    "<div class='panel-heading' id='{{vm.fieldName}}'><i class='fa fa-{{vm.icon}} fa-{{vm.iconSize}}x'></i>{{vm.title}}</div>" +
     "<div class='panel-body' ng-transclude></div>" +
     "</div>",
 };
