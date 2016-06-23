fd8799ece9dd849e436dbd738e0adfc31bb7100c
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-06-23 14:28:12 +0100

    Added My filter textbox

--- web/bower_components/my-angular-components/dist/myComponents.js
+++ web/bower_components/my-angular-components/dist/myComponents.js
@@ -152,10 +152,10 @@
 	        }
 	    },
 	    template: ["<div class='form-group'>",
-	        " <label class='control-label'  style='min-width: 100px !important;' >{{vm.fieldLabel}}</label>",
-	        " <div class='input-group'>",
+	        " <label class='control-label'  style='min-width: 80px !important;' >{{vm.fieldLabel}}</label>",
+	        " <div class='input-group'  style='width: 80% !important;'>",
 	        " <input type='text'  class='form-control' id='{{vm.fieldName}}' ng-model='vm.ngModel' ng-required='{{ vm.required }}'>",
-	        "   <div class='input-group-addon' ng-show='vm.showToolTip'><my-popover ng-model='vm.toolTip'></my-popover></div>",
+	        "   <div class='input-group-addon' style='line-height: 0 !important;'  ng-show='vm.showToolTip'><my-popover ng-model='vm.toolTip'></my-popover></div>",
 	        " </div>",
 	        "</div>"].join("")
 	};
