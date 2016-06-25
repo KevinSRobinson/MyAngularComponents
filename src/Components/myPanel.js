1ad9bd257e6abc3ff8974519151ff0382bea936d
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-06-25 16:04:02 +0100

    Added Panel Buttons

--- src/Components/myPanel.js
+++ src/Components/myPanel.js
@@ -5,13 +5,59 @@ var myPanel = {
         fieldName: "@",
         style: "@",
         icon: "@",
-        iconSize: "@"
+        iconSize: "@",
+        smallHeading: "@",
+        showAddButton: "@",
+        showEditButton: "@",
+        add:"&",
+        edit:"&"
     },
     controllerAs:"vm",
+    controller: function () {
+      "use strict";
+
+        var vm = this;
+        //vm.headingStyle = "padding: 10px 15px !important; ";
+
+        vm.init  = function () {
+                
+        };
+        
+        vm.setDefaults = function () {
+            if(vm.showAddButton == undefined){
+                vm.showAddButton = false;
+            }
+
+            if(vm.showEditButton == undefined){
+                vm.showEditButton = false;
+            }
+        };
+        
+        vm.getPanelStyle = function () {
+            if(vm.smallHeading != undefined){
+                return  "padding: 3px 5px !important; ";
+            }
+            return  "padding: 10px 15px";
+        };
+        
+        vm.getButtonStyle = function () {
+            if(vm.smallHeading != undefined){
+                return  "margin-left: 5px; padding: 3px;";
+            }
+            return  "margin-left: 5px; padding: 10px;";
+        };
+        
+    },
     template: "<div class='panel panel-{{vm.style}}'>" +
-    "<div class='panel-heading' id='{{vm.fieldName}}'><i class='fa fa-{{vm.icon}} fa-{{vm.iconSize}}x'></i>{{vm.title}}</div>" +
+    "<div class='panel-heading' style='{{vm.getPanelStyle()}}' id='{{vm.fieldName}}'><i class='fa fa-{{vm.icon}} fa-{{vm.iconSize}}x'></i>" +  
+        // title
+        "<span style='padding-left: 10px; font-weight: 700'>{{vm.title}}</span>" +
+        //buttons/
+        "<div ng-show='vm.showAddButton' ng-click='vm.add()' style='{{vm.getButtonStyle()}}' class='btn btn-default pull-right' style='padding: 3px;'><i class='fa fa-plus'></i> </div>" +
+        "<div ng-show='vm.showEditButton' ng-click='vm.edit()' style='{{vm.getButtonStyle()}}'  class='btn btn-default pull-right' style='padding: 3px;'><i class='fa fa-bars'></i> </div>" +
+    "</div>" + 
     "<div class='panel-body' ng-transclude></div>" +
-    "</div>",
+    "</div>"
 };
 
 angular.module("myComponents").component("myPanel", myPanel);
