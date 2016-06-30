bc39ccc6a88b4901df475236c738052301ed2a24
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-06-30 14:50:27 +0100

    trying to fix datefiled

--- src/Components/Dates/myDateField.js
+++ src/Components/Dates/myDateField.js
@@ -1,45 +1,44 @@
 var myDateField = {
-      
-  bindings: {
-    ngModel: "=",
-    fieldName: "=",
-    fieldLabel: "@"
-  },
-  template: [
-    "<div class='form-group'>",
-    "<label class='control-label' style='min-width: 110px; text-align: left'>{{$ctrl.fieldLabel}}</label>",
-    "<div class='input-group'>",
-    "<input type='text' class='form-control'",
-    " uib-datepicker-popup='dd-MMMM-yyyy' ng-model='$ctrl.ngModel'  is-open='$ctrl.popup1.opened'/> ",
-    "<span class='input-group-btn'>",
-    "<button type='button' class='btn btn-default' ng-click='$ctrl.open()'>" +
-    "<i class='fa fa-calendar'></i></button>",
-    " </span>",
-    "</div>"].join(""),
-  controller: function () {
-    // this.opened = false;
-    //
-    this.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    this.format = this.formats[0];
 
-    this.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1,
-      minDate: new Date()
-    };
+    bindings: {
+        ngModel: "=",
+        fieldName: "@",
+        fieldLabel: "@"
+    },
+    controllerAs: "vm",
+    template: [
+        "<div class='form-group'>",
+        "<label class='control-label' style='min-width: 110px; text-align: left'>{{vm.fieldLabel}}</label>",
+        "<div class='input-group'>",
+        "<input type='text' class='form-control'",
+        " uib-datepicker-popup='dd-MMMM-yyyy' ng-model='vm.ngModel'  is-open='vm.popup1.opened'/> ",
+        "<span class='input-group-btn'>",
+        "<button type='button' class='btn btn-default' ng-click='vm.open()'>" +
+        "<i class='fa fa-calendar'></i></button>",
+        " </span>",
+        "</div>"].join(""),
+    controller: function () {
+        "use strict";
+        var vm = this;
 
-    this.popup1 = {
-      opened: false
-    };
+        vm.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1,
+            minDate: new Date()
+        };
 
-    this.open = function () {
-      this.popup1.opened = true;
-    };
+        vm.popup1 = {
+            opened: false
+        };
 
-  }
+        vm.open = function () {
+            vm.popup1.opened = true;
+        };
+
+    }
 };
 
 
 var app = angular.module('myComponents')
-  .component('myDateField', myDateField);
+    .component('myDateField', myDateField);
 
