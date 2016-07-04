2ce8f1d237c250f1938a29ca830c2a19a7890423
ormeau@volunteernow.co.uk <ormeau@volunteernow.co.uk>
2016-07-04 12:01:38 +0100

    Added Id to Select List

--- dist/myComponents.js
+++ dist/myComponents.js
@@ -1332,24 +1332,25 @@
 /***/ function(module, exports) {
 
 	var mySelectList = {
-	  bindings: {
-	    ngModel: "=",
-	    items: "=",
-	    fieldLabel: "@"
-	  },
-	  controller: function(){
+	    bindings: {
+	        ngModel: "=",
+	        items: "=",
+	        fieldLabel: "@",
+	        fieldName: "@"
+	    },
+	    controllerAs: "vm",
+	    controller: function () {
 
-	  },
-	  template: ["<div class='form-group'>",
-	                " <label class='control-label' style='min-width: 110px; text-align: left'>{{$ctrl.fieldLabel}}</label>",
-	                  "<select ng-model='$ctrl.ngModel' class='form-control'>",
-	                " <option ng-repeat='option in $ctrl.items' value='{{option.Id}}'>{{option.Name}}</option>",
-	                  "</select>",
-	            "</div>"].join("")
+	    },
+	    template: ["<div class='form-group'>",
+	        " <label class='control-label' style='min-width: 110px; text-align: left'>{{vm.fieldLabel}}</label>",
+	        "<select ng-model='vm.ngModel' class='form-control' id='{{vm.fieldName}}'>",
+	        " <option ng-repeat='option in vm.items' value='{{option.Id}}'>{{option.Name}}</option>",
+	        "</select>",
+	        "</div>"].join("")
 
 	};
 	// convert-to-number
-
 
 
 	angular.module("myComponents").component("mySelectList", mySelectList);
