872e70774d8c892e22dd60fb1f54fbedb2df9721
seamusjunior44 <kevin.robinson@outlook.com>
2016-11-26 18:12:42 +0000

    TEst

---
+++ syncfusion.js
@@ -0,0 +1,8 @@
+require("./src/SyncFusion/jquery.easing.1.3.min.js");
+require("./src/SyncFusion/jsrender.min.js");
+require("./src/SyncFusion/ej.core.min.js");
+require("./src/SyncFusion/ej.web.all.min.js");
+require("./src/SyncFusion/ej.widget.angular.min.js");
+
+require("./src/SyncFusion/flat-azure/min/ej.theme.min.css");
+require("./src/SyncFusion/flat-azure/min/ej.widgets.core.min.css");
